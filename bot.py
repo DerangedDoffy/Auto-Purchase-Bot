f6e4cab6b8777f0c4527a3afc6fe951f9a8383ff
DerangedDoffy <80848308+DerangedDoffy@users.noreply.github.com>
2021-04-20 22:54:59 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,26 @@
+# Log into the site with product 
+# Find product under X amount 
+# If product unavailable, wait until availability 
+# Add product to cart 
+# Add payment 
+# Checkout 
+
+import time
 from selenium import webdriver 
 
-driver = webdriver.Chrome(executable_path = "D:\\Downloads\\chromedriver_win32\\chromedriver.exe")
+# For using Chrome
+browser = webdriver.Chrome('D:\\Downloads\\chromedriver_win32\\chromedriver.exe')
 
+# BestBUy RTX 3080 page 
+browser.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")
+
+buyButton = False
+
+while not buybutton: 
+    try:
+        addToCartBtn = addButton = browser.find_element_by_class_name("btn-disabled")
+        print("Button isn't ready yet.")
+        time.sleep(1)
+        browser.refresh()
+    except:
+        addToCartBtn = addButton = browser.find_element_by_class_name("btn-primary")
